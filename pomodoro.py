0c7c846e07f61aad0da20f958b6de69f61ab5a56
Brandon <bran112299@yahoo.com>
2020-08-16 22:56:44 -0400

    accident

--- pomodoro.py
+++ pomodoro.py
@@ -77,7 +77,7 @@ class Timers():
     
     
     def cancel(self):
-        '''Cancels any current after jobs'''
+        '''Cancels any current after jobs, useful for new iter'''
         
         if self._job is not None:
             app.after_cancel(self._job)
@@ -89,40 +89,42 @@ class Timers():
     
     def countdown(self, t = None):
         '''Counts down from a start number, optionality for modes'''
-        if t is not None:
+        if t is not None: # As long as num is not none
             self.t_d = t
             
-        if self.t_d <= 0:
-            # No Mode
+        if self.t_d >= 1: # > 0
+            mins, secs = divmod(self.t_d, 60) #returns tuple num & denom
+            timeformat = '{:02d}:{:02d}'.format(mins, secs) #02d formats an integer (d) to a field of minimum width 2 (2), with zero-padding on the left (leading 0):
+            self.changeTime(timeformat) #carriage to replcae
+            self.t_d -= 1
+            self._job = app.after(1000, self.countdown)  
+        
+        # When timer reaches 0
+        else:
             if self.mode is None:
                 return
 
-            # Pomodoro Mode
+            # If Pomodoro Mode
             elif self.mode == "Pomodoro":
-                    if self.brake is False:
-                        self.brake = True
-                        self.mode = "Break"
-                        self.changeTime("Moving to break")
-                        app.update()
-                        time.sleep(1)
-                        self.countdown(self.stop)
-                        return
-            elif self.mode == "Break":
-                    self.changeTime("Break Over")
+                    self.brake = True # Sets to break mode
+                    self.mode = "Break"
+                    self.changeTime("Moving to break") #Update text
+                    app.update()
+                    time.sleep(1)
+                    self.countdown(self.stop) # Starts again from break num (gotta change that name)
+                    return
+                
+            elif self.mode == "Break": # If reaches 0 on break mode
+                    self.changeTime("Break Over") # Resets everything for new iteration
                     self.mode = None
-                    self.brake = False
+                    self.brake = False    ######### JUST ADD RESET FUNCTION
                     self.t_d = None
                     #self.t_d = None
                     #else:
                      #   self.changeTime("Break Over")
                      # 3  self.t_d = None
         
-        else:
-            mins, secs = divmod(self.t_d, 60) #returns tuple num & denom
-            timeformat = '{:02d}:{:02d}'.format(mins, secs) #02d formats an integer (d) to a field of minimum width 2 (2), with zero-padding on the left (leading 0):
-            self.changeTime(timeformat) #carriage to replcae
-            self.t_d -= 1
-            self._job = app.after(1000, self.countdown)  
+        
                 
     def countup(self):
         '''Counts up from a 0'''
@@ -135,19 +137,20 @@ class Timers():
     
     def pomodoro(self):
         '''Pomodoro mode'''
-        self.cancel()
+        self.cancel() # Makes sure no ongoing jobs
         
-        if self.brake is not True:
-            self.mode = "Pomodoro"
+        if self.brake is False: # Not in break mode (could be unproductive)
+            self.mode = "Pomodoro" # Set to pomo
             print("Changed")
         else:
-            self.mode = "Break"
-        if self.t_d is None:
-            self.start = int(app.pomostart.get())
+            self.mode = "Break" # If it is still in break mode and timer never reached 0, set back
+            
+        if self.t_d is None: # If no numbers counting down
+            self.start = int(app.pomostart.get()) # Fetch blocks
             self.stop = int(app.pomostop.get())
-            self.brake = False
-            self.countdown(self.start)
-        else:
+            self.brake = False # Ensure brake is off (Neccesary??)
+            self.countdown(self.start) 
+        else: # Still numbers left in queue means continuing from another mode, so add 1 (cause the wait is 1)
             self.countdown(self.t_d+1)
             print(self.mode)
         #Start:
@@ -166,6 +169,7 @@ class Timers():
         '''Text updater'''
         app.text.set(self.mode)
         app.time.set(i)   
+        # break check here to clean it?
 
 #ExampleApp()
 if __name__ == "__main__":
